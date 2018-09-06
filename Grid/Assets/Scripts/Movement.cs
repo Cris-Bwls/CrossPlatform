a9bd8b4cbaa47e9ee95374db3988dd995de083bd
2Morro <43025906+2Morro@users.noreply.github.com>
2018-09-06 16:10:22 +1000

    Initial Commit

---
+++ Grid/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    private Vector3 pos;
+    //private Transform trans;
+    bool rotated = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos = transform.position;
+        //trans = transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.D) /*&& trans.position == pos*/)
+        {
+            if (rotated == false)
+            {
+                transform.Rotate(0, 90, 0);
+                rotated = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A) /*&& trans.position == pos*/)
+        {
+            if (rotated == false)
+            {
+                transform.Rotate(0, -90, 0);
+                rotated = true;
+            }
+        }
+        if (pos == transform.position)
+        {
+            pos += transform.forward;
+            rotated = false;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
---
+++ Grid/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public ParticleSystem particleSystem;
+
+	// Use this for initialization
+	void Awake ()
+    {
+	}
+	
+
+	void OnCollisionEnter(Collision collision)
+    {
+        var collider = collision.collider;
+
+        if (collider.tag != "Ground")
+        {
+            particleSystem.Play();
+        }
+	}
+}
